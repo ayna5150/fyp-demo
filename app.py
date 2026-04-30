9cbf63626d7d35bdb4f32abf426d5ab8bbe457ea
Moza Amur <mozaamur1602@gmail.com>
2026-04-30 12:51:42 +0400

    Refactor HTML generation for model display

--- app.py
+++ app.py
@@ -689,8 +689,14 @@ with col_info:
 ''', unsafe_allow_html=True)
 
     # Models
+    
+    
     st.markdown(
-        f'<div class="sb-title" style="margin-top:1rem">{T["sidebar_models"]}</div>',
+        f'''
+        <div class="sb-title" style="margin-top:1rem">
+            {T["sidebar_models"]}
+        </div>
+        ''',
         unsafe_allow_html=True
     )
     
@@ -702,14 +708,19 @@ with col_info:
     
         model_class = "tox-model" if "AraBERT v2" in name else "pii-model"
     
-        models_html += f'''
+        models_html += f"""
         <div class="sb-model">
-            <span class="sb-model-name {model_class}">{name}</span>
-            <span class="sb-model-desc">{dot} {desc}</span>
-        </div>
-        '''
+            <span class="sb-model-name {model_class}">
+                {name}
+            </span>
     
-    models_html += '</div>'
+            <span class="sb-model-desc">
+                {dot} {desc}
+            </span>
+        </div>
+        """
+    
+    models_html += "</div>"
     
     st.markdown(models_html, unsafe_allow_html=True)
 
